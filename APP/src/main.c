fbc0841b1912984b04bcbb9520090f829604ba84
Lovisa Helgadottir <loairpa@gmail.com>
2013-07-12 11:02:19 +0200

    Added license main.c

--- APP/src/main.c
+++ APP/src/main.c
@@ -1,8 +1,25 @@
-/************************* (C) COPYRIGHT 2010 ROBOTIS **************************
+/************************* Copyright LIH/ HM  **************************
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+*****************************************************************************/
+
+/*****************************************************************************
+
  * File Name          : main.c
- * Author             : danceww
+ * Author             : Lovisa Irpa Helgadottir / Hamid Mobalegh
  * Version            : V0.0.1
- * Date               : 08/23/2010
+ * Date               : 30/06/2013
  * Description        : Main program body
  *******************************************************************************/
 
